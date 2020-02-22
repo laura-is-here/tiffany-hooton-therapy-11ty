a99882f2e8eb72c9e5a54cd15e783252f98d76da
laura <lauras_here@pm.me>
2020-02-21 19:15:00 -0800

    Modify purgecss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: ["./src/site/**/*.njk"],
 
-  whitelist: ["md:hidden"],
+  whitelist: ["md:hidden", "hidden"],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
